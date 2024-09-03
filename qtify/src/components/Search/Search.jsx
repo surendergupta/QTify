bf1a4dd5488e15b1004d58ad21e832257b08d074
myinrbtc <156459968+myinrbtc@users.noreply.github.com>
2024-09-03 18:28:22 +0530

    C4:Search Bar

--- qtify/src/components/Search/Search.jsx
+++ qtify/src/components/Search/Search.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { ReactComponent as SearchIcon } from '../../assets/search-icon.svg';
 
+import styles from './Search.module.css';
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className={ styles.searchContainer}>
+      <input type='text' className={ styles.searchInput } placeholder='Search a album of your choice' />
+      <button type='button' className={styles.searchBtn}>
+        <SearchIcon />
+      </button>
+    </div>
   )
 }
 
